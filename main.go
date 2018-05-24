91a6a884212eeb1a8b00a397f401dd7f8c8e013c
Yusei Yamanaka <yusei1128@gmail.com>
2018-05-25 03:34:55 +0900

    Update config

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("I am miyukki! Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
+	fmt.Fprintf(w, "<h1>Master Branch!</h1>\n")
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
